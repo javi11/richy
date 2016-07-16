a312d299c86ab0539688aad6f5a651bb0909313a
Víctor Boza Martín <Vctor Boza Martn>
2016-07-16 03:14:22 +0200

    El mapa estaba mal, lo he cambiadoyhepuesto uno de prueba, ahora se ve todo mejor. Le he añadido los bloques que se rompen, la animación y el scrip para que se borren.

---
+++ juego_richy/Assets/Scripts/Player/bloke_rompe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bloke_rompe : MonoBehaviour {
+
+    public GameObject explosion;
+
+	void OnTriggerEnter2D( Collider2D other)
+    {
+        if (other.gameObject.tag == "rompe")
+        {
+            Instantiate(explosion, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+    }
+	
+}
---
+++ juego_richy/Assets/Scripts/Player/destruir.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destruir : MonoBehaviour {
+    Animator mianimacion;
+    float dura;
+	// Use this for initialization
+	void Start () {
+        mianimacion = this.GetComponent<Animator>();
+        RuntimeAnimatorController ac = mianimacion.runtimeAnimatorController;
+        for (int i=0; i < ac.animationClips.Length; i++)
+        {
+            if(ac.animationClips[i].name =="explosion")
+            {
+                dura = ac.animationClips[i].length;
+            }
+        }
+        StartCoroutine(Destroytimer());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Destroytimer()
+    {
+        yield return new WaitForSeconds(dura);
+        Destroy(this.gameObject);
+    }
+}
